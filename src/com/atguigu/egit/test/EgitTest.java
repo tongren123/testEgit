e3ac065440718d58c31f0a13d8114941c78824c9
tr888 <tr@qq.com>
2019-07-16 18:33:17 +0800

    666确定更新

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -3,6 +3,6 @@ package com.atguigu.egit.test;
 public class EgitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
-		System.out.println("bbbbb");
+		System.out.println("AAAAA");
 	}
 }
